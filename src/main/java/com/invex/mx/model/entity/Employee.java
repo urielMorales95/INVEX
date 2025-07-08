a3900e107359951789c478960731891793b52837
CommonMS01 <109167461+CommonMS01@users.noreply.github.com>
2025-07-07 23:55:46 -0600

    cambios de CI

--- src/main/java/com/invex/mx/model/entity/Employee.java
+++ src/main/java/com/invex/mx/model/entity/Employee.java
@@ -15,12 +15,10 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Builder
 @Table(name = "EMPLOYEE")
